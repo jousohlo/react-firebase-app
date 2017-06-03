abeb8c5f1c5ca8f68987b9789eec956533621100
Jouko Sohlo <jouko.sohlo@gmail.com>
2017-06-04 00:54:43 +0300

    added google sign in

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
 import fire from './fire';
+import firebase from 'firebase'
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { messages: [] }; // <- set up react state
-    this.addMessage = this.addMessage.bind(this);
-    this.clearMessages = this.clearMessages.bind(this);
+    this.state = { 
+      messages: [],
+      signedIn: 'Kirjaudu'
+      }; // <- set up react state
   }
+
+
   componentWillMount(){
     console.log('componentWillMount');
+    fire.auth().onAuthStateChanged(user => {
+      if (user !== null) {
+        this.setState({signedIn: 'Kirjautunut: ' + user.displayName});
+      }
+    });
     /* Create reference to messages in Firebase Database */
     let messagesRef = fire.database().ref('messages').orderByKey().limitToLast(100);
     messagesRef.on('child_added', snapshot => {
@@ -22,21 +31,47 @@ class App extends Component {
       this.setState({ messages: [] });
     })
   }
+
   addMessage(e){
     e.preventDefault(); // <- prevent form submit from reloading the page
     /* Send the message to Firebase */
     fire.database().ref('messages').push( this.inputEl.value );
     this.inputEl.value = ''; // <- clear the input
   }
+
   clearMessages(){
     fire.database().ref('messages').set( null );
     this.setState({ messages: [] });
   }
 
+  authenticate() {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    fire.auth().signInWithRedirect(provider);
+    fire.auth().getRedirectResult().then(function(result) {
+      if (result.credential) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // ...
+      }
+      // The signed-in user info.
+      //var user = result.user;
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      console.log('Authentication error: ' + errorMessage);
+      // ...
+    });
+  } 
+
   render() {
     return (
       <div className='page'>
-        <form onSubmit={this.addMessage}>
+        <form onSubmit={this.addMessage.bind(this)}>
           <input type="text" ref={ el => this.inputEl = el } />
           <input type="submit" value="Tallenna"/>
         </form>
@@ -47,7 +82,8 @@ class App extends Component {
             }
           </ul>
         </div>
-        <button onClick = {this.clearMessages}>Tyhjennä</button>
+        <button onClick = {this.clearMessages.bind(this)}>Tyhjennä</button>
+        <button onClick = {this.authenticate.bind(this)}>{this.state.signedIn}</button>
       </div>
     );
   }
